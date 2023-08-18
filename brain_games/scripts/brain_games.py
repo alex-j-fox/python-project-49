d898b0c20d4de4a8c853237d631d49de498a0b03
alex-j-fox <alex-j-fox@yandex.ru>
2023-08-18 22:44:58 +0300

    update cli.py, brain_games.py
    fixed errors detected by linter

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 from brain_games.cli import welcome_user
 
+
 def main():
-    print(f'Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')
     print(welcome_user())
+
 
 if __name__ == '__main__':
     main()
