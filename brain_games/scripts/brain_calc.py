3199dc2ef463b008dede9e1bc16334e4dc300373
alex-j-fox <alex-j-fox@yandex.ru>
2023-08-29 00:58:53 +0300

    add brain_calc.py
    Added brain_calc.py
    Updated brain_even.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
 from brain_games.engine import run_game
-import brain_games.games.even as even
+import brain_games.games.calc as calc
 
 
 def main():
-    run_game(even)
+    run_game(calc)
 
 
 if __name__ == '__main__':
