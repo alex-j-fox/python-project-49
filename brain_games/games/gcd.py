df0edf056529f17718c789792699854404922622
alex-j-fox <alex-j-fox@yandex.ru>
2023-08-29 23:35:26 +0300

    update gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -9,18 +9,15 @@ def get_question_and_answer():
     a = get_random_number()
     b = get_random_number()
     question = f'{a}, {b}.'
-    if a == b:
-        return question, a
-    else:
-        while a != b:
-            if a == 0 or b == 0:
-                break
-            elif a > b:
-                a = a % b
-            elif b > a:
-                b = b % a
-        answer = max(a, b)
-        return question, answer
+    while a != b:
+        if a == 0 or b == 0:
+            break
+        elif a > b:
+            a = a % b
+        elif b > a:
+            b = b % a
+    answer = max(a, b)
+    return question, answer
 
 
 def show_question():
