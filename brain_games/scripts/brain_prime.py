61bc463707a35f4207fa59817a90443159e6c7c1
alex-j-fox <alex-j-fox@yandex.ru>
2023-08-31 22:54:16 +0300

    add prime.py, brain_prime.py

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from brain_games.engine import run_game
+import brain_games.games.prime as prime
+
+
+def main():
+    run_game(prime)
+
+
+if __name__ == '__main__':
+    main()
