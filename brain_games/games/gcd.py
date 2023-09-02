076be3c02793a7e54dbec9aa600d068d1092ecd2
alex-j-fox <alex-j-fox@yandex.ru>
2023-09-02 08:15:06 +0300

    fix gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -4,7 +4,7 @@ from random import randint
 def get_question_and_answer():
     a = randint(0, 100)
     b = randint(0, 100)
-    question = f'{a}, {b}.'
+    question = f'{a} {b}.'
     while a != b:
         if min(a, b) == 0:
             return question, max(a, b)
